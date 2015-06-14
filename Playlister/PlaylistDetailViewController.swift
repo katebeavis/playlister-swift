44eb7bdb8e09828cccf29b3715df8bb581db4ec6
katebeavis <katebeavis84@gmail.com>
2015-06-14 19:38:16 +0100

    first commit

---
+++ Playlister/PlaylistDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaylistDetailViewController.swift
+//  Playlister
+//
+//  Created by Kate Beavis on 14/06/2015.
+//  Copyright (c) 2015 Kate Beavis. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
