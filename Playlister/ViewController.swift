e06aff23768e2159fc4150b9782df5dcadbd7205
katebeavis <katebeavis84@gmail.com>
2015-06-14 17:01:20 +0100

    Initial Commit

---
+++ Playlister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Playlister
+//
+//  Created by Kate Beavis on 14/06/2015.
+//  Copyright (c) 2015 Kate Beavis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
