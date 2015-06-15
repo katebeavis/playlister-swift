44cedbb083ed71dc4d883b790f83c79560ccc5b5
katebeavis <katebeavis84@gmail.com>
2015-06-15 20:58:38 +0100

    Multiple images added to master view

--- Playlister/PlaylistMasterViewController.swift
+++ Playlister/PlaylistMasterViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class PlaylistMasterViewController: UIViewController {
     
+    var playlistsArray: [UIImageView] = []
     @IBOutlet weak var playlistImageView0: UIImageView!
+    @IBOutlet weak var playlistImageView1: UIImageView!
+    @IBOutlet weak var playlistImageView2: UIImageView!
+    @IBOutlet weak var playlistImageView3: UIImageView!
+    @IBOutlet weak var playlistImageView4: UIImageView!
+    @IBOutlet weak var playlistImageView5: UIImageView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let playlist = Playlist(index: 0)
-        playlistImageView0.image = playlist.icon
-        playlistImageView0.backgroundColor = playlist.backgroundColor
+        playlistsArray += [playlistImageView0, playlistImageView1, playlistImageView2, playlistImageView3, playlistImageView4, playlistImageView5]
+        
+        for index in 0..<playlistsArray.count {
+            let playlist = Playlist(index: index)
+            let playlistImageView = playlistsArray[index]
+            
+            playlistImageView.image = playlist.icon
+            playlistImageView.backgroundColor = playlist.backgroundColor
+            
+        }
         
     }
 
@@ -28,16 +42,28 @@ class PlaylistMasterViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showPlaylistDetailSegue" {
-            let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
-            playlistDetailController.playlist = Playlist(index: 0)
+            
+            let playlistImageView = sender!.view as! UIImageView
+            if let index = find(playlistsArray, playlistImageView) {
+                let playlistDetailController = segue.destinationViewController as! PlaylistDetailViewController
+                playlistDetailController.playlist = Playlist(index: index)
+            }
+            
         }
     }
+    
     
     @IBAction func showPlaylistDetail(sender: AnyObject) {
         
         performSegueWithIdentifier("showPlaylistDetailSegue", sender: sender)
         
     }
+    
+//    @IBAction func showPlaylistDetail(sender: AnyObject) {
+//        
+//        performSegueWithIdentifier("showPlaylistDetailSegue", sender: sender)
+//        
+//    }
     
 }
 
